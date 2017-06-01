4d8876834aea99ab40b101fb5f93001aa9c2103f
Elhior <Arhangelc@rambler.ru>
2017-06-01 17:34:00 +0300

    UnitTests initialised

---
+++ BeeGame.UnitTests/GameInstanseTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bee_game.Models;
+using NUnit.Framework;
+
+namespace BeeGame.UnitTests
+{
+    [TestFixture]
+    public class GameInstanseTests
+    {
+        //ЧтоТестируем_СостояниеТеста_ОжидаемоеПоведение 
+        [Test]
+        public static void GetNewGameId_Expected0()
+        {
+            int expected = 0;
+            int result = GameInstance.GetNewGameId();
+
+            Assert.AreEqual(expected, expected);
+        }
+        [Test]
+        public static void Test()
+        {
+            int expected = 2;
+            int result = GameInstance.GetNewGameId();
+
+            Assert.AreEqual(result, expected);
+        }
+    }
+}
