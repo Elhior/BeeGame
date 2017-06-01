9c13134eaefed033599e768e179a0a6697ee17f6
Elhior <Arhangelc@rambler.ru>
2017-06-01 17:37:43 +0300

    Interfaces for layers added

---
+++ Bee game/DAL/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bee_game.Models;
+
+namespace Bee_game.DAL
+{
+    interface IRepository<T> : IDisposable
+                where T : class
+    {
+        //saving game
+        bool Save(T game);
+        //loading game
+        bool Load(T game);
+    }
+}
---
+++ Bee game/Service/IService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bee_game.Models;
+
+namespace Bee_game.Service
+{
+    interface IService
+    {
+        //initialise new game
+        void NewGame(int gameID);
+        //saving game
+        void SaveGame(int gameID, string savingType);
+        //loading game
+        string LoadGame(int gameID ,string loadingType);
+        //save new game configuration
+        void SaveConfiguration(int gameID, GameConfiguration gameConfiguration);
+        //return game configuration ????????return type config
+        string GetConfiguration(int gameID);
+        //hit target(chosen or random)
+        string HitBee(int gameID, string target);
+    }
+}
