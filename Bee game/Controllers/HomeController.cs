d5673d59cc60b017ea4484690dc527fa8b68a350
Elhior <Arhangelc@rambler.ru>
2017-06-01 19:39:04 +0300

    rest methods are replaced to RESTController

--- Bee game/Controllers/HomeController.cs
+++ Bee game/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ namespace Bee_game.Controllers
     {
         public ActionResult Index()
         {
+            Logger.InitLogger();
             return View();
         }
 
@@ -16,43 +17,7 @@ namespace Bee_game.Controllers
         {
             return View(GameInstance.getInstance(GetBrowserId()).Stage);
         }
-
-        [HttpGet]
-        public void NewGame()
-        {
-            BeeGameService.Service().NewGame(this);
-        }
-
-        [HttpGet]
-        public void SaveGame(string savingtype)
-        {
-            BeeGameService.Service().SaveGame(this, savingtype);
-        }
-
-        [HttpGet]
-        public string LoadGame(string loadingtype)
-        {
-            return BeeGameService.Service().LoadGame(this, loadingtype);
-        }
-
-        [HttpGet]
-        public string GetConfiguration()
-        {
-            return BeeGameService.Service().GetConfiguration(this);
-        }
-
-        [HttpPost]
-        public void SaveConfiguration(GameConfiguration gameConfiguration)
-        {
-            BeeGameService.Service().SaveConfiguration(this,gameConfiguration);
-        }
-
-        [HttpGet]
-        public string HitBee(string id)
-        {
-            return BeeGameService.Service().HitBee(this, id);
-        }
-
+        
         //get unique id of browser to run new games from different browsers simultaneously
         public int GetBrowserId()
         {
