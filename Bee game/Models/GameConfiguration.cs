34c73d8df8d73a83455a9dc276f2f59af83e40e7
Elhior <Arhangelc@rambler.ru>
2017-06-06 16:06:44 +0300

    Constants of game configuration are now in configuration file

--- Bee game/Models/GameConfiguration.cs
+++ Bee game/Models/GameConfiguration.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Configuration;
 
 namespace Bee_game.Models
 {
@@ -55,15 +56,15 @@ namespace Bee_game.Models
         // default game configuration
         public GameConfiguration()
         {
-            QueensNumber = 1;
-            WorkersNumber = 5;
-            DronesNumber = 8;
-            QueensLifespan = 100;
-            QueensHitpoints = 8;
-            WorkersLifespan = 75;
-            WorkersHitpoints = 10;
-            DronesLifespan = 50;
-            DronesHitpoints = 12;
+            QueensNumber = Int32.Parse(ConfigurationManager.AppSettings["QueensNumber"]);
+            WorkersNumber = Int32.Parse(ConfigurationManager.AppSettings["WorkersNumber"]);
+            DronesNumber = Int32.Parse(ConfigurationManager.AppSettings["DronesNumber"]);
+            QueensLifespan = Int32.Parse(ConfigurationManager.AppSettings["QueensLifespan"]);
+            QueensHitpoints = Int32.Parse(ConfigurationManager.AppSettings["QueensHitpoints"]);
+            WorkersLifespan = Int32.Parse(ConfigurationManager.AppSettings["WorkersLifespan"]);
+            WorkersHitpoints = Int32.Parse(ConfigurationManager.AppSettings["WorkersHitpoints"]);
+            DronesLifespan = Int32.Parse(ConfigurationManager.AppSettings["DronesLifespan"]);
+            DronesHitpoints = Int32.Parse(ConfigurationManager.AppSettings["DronesHitpoints"]);
         }
     }
 }
